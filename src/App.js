f311037fe443b91b486e5e5632e85679af776901
dev <debiduttaacharya2002@gmail.com>
2022-10-06 11:19:20 +0530

    pushing code4

--- src/App.js
+++ src/App.js
@@ -5,13 +5,21 @@ function App() {
 
   const name = "Dev";
   const arr = [1, 2, 3, 4, 5];
-  const viewed = 50;
+  const viewed = 20;
+  // const obj = {n: 'dev', age:20};
+
+  const link = "https://www.google.com";
 
   return (
     <div className="App">
       <h1>This is {name}</h1>
       <h2>Arr = {arr} </h2>
       <h2>{name} viewed your profile {viewed} times</h2>
+      {/* <p>{ obj }</p> */}
+
+      <p> {Math.floor(Math.random() * (6 - 1 + 1)) + 1 } </p>
+
+      <a href={link}>Google</a>
     </div>
   );
 }
