5105d972ae6268940ecf0ca864a62b9c0af7f3e3
dev <debiduttaacharya2002@gmail.com>
2022-10-06 22:45:28 +0530

    code8

--- src/Home.js
+++ src/Home.js
@@ -2,19 +2,32 @@ import { useState } from "react";
 
 const Home = () => {
 
+    const prevName = "Mohnish";
+    const afterName = "D3v";
+
     // let name = "Mario";
-    const [name, setName] = useState('Mario');
-    const [age, setAge] = useState('25');
+    const [name, setName] = useState(prevName);
+    const [age, setAge] = useState('19');
+    const [n, setN] = useState(0);
 
     const handleClick = () => {
-        setName('Dev');
-        setAge('19');
+        if(n%2 === 0){ 
+            setName(afterName);
+            setAge('19');
+            setN(n+1);
+        }
+        else{
+            setName(prevName);
+            setAge('20');
+            setN(n+1);
+        }
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
             <p> {name} is {age} years old..</p>
+            <p>count = { n }</p>
             <button onClick={handleClick}> click me</button>
         </div>
     );
