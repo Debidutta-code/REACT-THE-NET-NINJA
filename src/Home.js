a11da117a3454663f6af58eadc73b7c6cb76b825
dev <debiduttaacharya2002@gmail.com>
2022-10-06 23:01:55 +0530

    .

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
+
+    const [blogs, setBlogs] = useState([
+        { title: "My new Website", body: "lorem ipsum...", author: "Mario", id: 1 },
+        { title: "Welcome Party", body: "lorem ipsum...", author: "Dev", id: 2 },
+        { title: "Web Dev Top Tips", body: "lorem ipsum...", author: "Mohnish", id: 3 }
+    ]);
+
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2> { blog.title } </h2>
+                    <p>Written by - { blog.author } </p>
+                </div>
+            ))}
         </div>
     );
 }
