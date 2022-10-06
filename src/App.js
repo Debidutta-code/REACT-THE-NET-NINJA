57d75252f681bfe7dd4b654a0fde60e93dac13f0
dev <debiduttaacharya2002@gmail.com>
2022-10-06 12:38:56 +0530

    pushing code7

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import React from 'react';
 import Navbar from './Navbar';
 import Home from './Home';
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
 const Home = () => {
+
+    const text1 = "click me";
+    const text2 = "click me again";
+
+    const handleClick = (e) => {
+        console.log('hello, Ninjas', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log("hello " + name, e.target);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}> {text1} </button>
+            <button onClick={(e) => {handleClickAgain("dev", e);}}> {text2} </button>
         </div>
     );
 }
