682edccca1bb87de89fc3b508663e876c72c9002
dev <debiduttaacharya2002@gmail.com>
2022-10-06 11:40:52 +0530

    pushing code5 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React from 'react';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
-        <h1>App Component</h1>
+        <Home />
       </div>
     </div>
   );
