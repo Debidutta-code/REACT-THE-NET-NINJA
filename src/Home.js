7cf782472ec01126140ac64421a681a71b1736f5
dev <debiduttaacharya2002@gmail.com>
2022-10-08 19:24:23 +0530

    commiting code12

--- src/Home.js
+++ src/Home.js
@@ -8,12 +8,13 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: "My new Website", body: "lorem ipsum...", author: "Mario", id: 1 },
         { title: "Welcome Party", body: "lorem ipsum...", author: "Dev", id: 2 },
-        { title: "Web Dev Top Tips", body: "lorem ipsum...", author: "Mohnish", id: 3 }
+        { title: "Web Dev Top Tips", body: "lorem ipsum...", author: "Mario", id: 3 }
     ]);
 
     return ( 
         <div className="home">
-            <BlogList blogs = {blogs} title = "This is the Title..." id = {id} />
+            <BlogList blogs = {blogs} title = "All Blog." id = {id} />
+            <BlogList blogs = {blogs.filter((blog) => blog.author === "Mario" )} title = "Mario's blog" id = {id} />
         </div>
     );
 }
