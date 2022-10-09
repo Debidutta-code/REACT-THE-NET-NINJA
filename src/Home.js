bbd3fe3410764ba76e0119033bd85b34e9a24200
dev <debiduttaacharya2002@gmail.com>
2022-10-09 10:42:30 +0530

    code19

--- src/Home.js
+++ src/Home.js
@@ -7,22 +7,32 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-                setIsPending(false);
-            });
+                .then(res => {
+                    if(!res.ok){
+                        throw Error("could not fetch data");
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setBlogs(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsPending(false);
+                    setError(err.message);
+                })
         }, 1000);
     }, []);
 
     return ( 
         <div className="home">
+            { error && <div> { error }</div>}
             { isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs = {blogs} title = "This is the Title..." id = {id}/>}
         </div>
