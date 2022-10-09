e5679b5d744d3c859e18294fe0d0fa2906353b7e
dev <debiduttaacharya2002@gmail.com>
2022-10-09 10:29:24 +0530

    code18

--- src/Home.js
+++ src/Home.js
@@ -6,19 +6,24 @@ const Home = () => {
     const id = 1;
 
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
             .then(res => {
                 return res.json();
             })
             .then(data => {
                 setBlogs(data);
+                setIsPending(false);
             });
+        }, 1000);
     }, []);
 
     return ( 
         <div className="home">
+            { isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs = {blogs} title = "This is the Title..." id = {id}/>}
         </div>
     );
